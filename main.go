6cd28c1d5f7f9b7e19e78355912200bd8dd0b289
eannchen <conspivit@gmail.com>
2024-01-27 23:44:56 +0800

    LoadFilteredPolicy

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/csv"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -50,59 +49,74 @@ func main() {
 	}
 
 	// e, err := casbin.NewEnforcer("model_my.conf", "policy_my.csv")
-	e, err := casbin.NewEnforcer("model_my.conf", adapter)
+	// e, err := casbin.NewEnforcer("model_my.conf", adapter)
+	e, err := casbin.NewEnforcer("model_my.conf")
 	if err != nil {
 		fmt.Errorf("casbin.NewEnforcer", err)
 	}
-	migrate(e)
 	e.SetFieldIndex("p", constant.SubjectIndex, 0)
 	e.SetFieldIndex("p", constant.DomainIndex, 1)
 	e.SetFieldIndex("p", constant.ObjectIndex, 2)
-	if err := e.LoadPolicy(); err != nil {
-		fmt.Errorf("LoadPolicy", err)
+
+	e.SetAdapter(adapter)
+
+	if err := e.GetAdapter().(*gormadapter.Adapter).LoadFilteredPolicy(e.GetModel(), gormadapter.Filter{
+		// V0: []string{"role:admin"},
+		V1: []string{"dom:admin"},
+		// V2: []string{"dom:admin"},
+		// V2: []string{"obj:account_admin"},
+		// V3: []string{"act:all"},
+		// V4: []string{"allow"},
+	}); err != nil {
+		fmt.Errorf("LoadFilteredPolicy", err)
 	}
+	// migrate(e) // will also load policy
 
-	// fmt.Println(e.GetPolicy())
-	// fmt.Println(e.GetNamedGroupingPolicy("g"))
-	// fmt.Println(e.GetNamedGroupingPolicy("g2"))
-	// fmt.Println(e.GetNamedGroupingPolicy("g3"))
+	// if err := e.LoadPolicy(); err != nil {
+	// 	fmt.Errorf("LoadPolicy", err)
+	// }
 
-	fmt.Println()
-	j, _ := json.Marshal(e.GetPermissionsForUserInDomain("user:ian", "dom:marketing"))
-	fmt.Println("GetPermissionsForUserInDomain: ", string(j))
+	fmt.Println(e.GetPolicy())
+	fmt.Println(e.GetNamedGroupingPolicy("g"))
+	fmt.Println(e.GetNamedGroupingPolicy("g2"))
+	fmt.Println(e.GetNamedGroupingPolicy("g3"))
 
-	fmt.Println()
-	j1, _ := json.Marshal(e.GetPermissionsForUserInDomain("user:ian", "dom:admin"))
-	fmt.Println("GetPermissionsForUserInDomain: ", string(j1))
+	// fmt.Println()
+	// j, _ := json.Marshal(e.GetPermissionsForUserInDomain("user:ian", "dom:marketing"))
+	// fmt.Println("GetPermissionsForUserInDomain: ", string(j))
 
-	fmt.Println()
-	j2, _ := json.Marshal(e.GetPermissionsForUser("user:ian", "dom:admin", "dom:marketing"))
-	fmt.Println("GetPermissionsForUser: ", string(j2))
+	// fmt.Println()
+	// j1, _ := json.Marshal(e.GetPermissionsForUserInDomain("user:ian", "dom:admin"))
+	// fmt.Println("GetPermissionsForUserInDomain: ", string(j1))
 
-	fmt.Println()
-	r, _ := e.GetRolesForUser("user:ian", "dom:admin")
-	fmt.Println("GetRolesForUser: ", r)
+	// fmt.Println()
+	// j2, _ := json.Marshal(e.GetPermissionsForUser("user:ian", "dom:admin", "dom:marketing"))
+	// fmt.Println("GetPermissionsForUser: ", string(j2))
 
-	fmt.Println()
-	r1, _ := e.GetRolesForUser("user:ian", "dom:marketing")
-	fmt.Println("GetRolesForUser: ", r1)
+	// fmt.Println()
+	// r, _ := e.GetRolesForUser("user:ian", "dom:admin")
+	// fmt.Println("GetRolesForUser: ", r)
 
-	fmt.Println()
-	if ok, err := e.Enforce("user:ian", "dom:admin", "obj:account_admin", "act:read"); err != nil {
-		fmt.Errorf("Enforce err: ", err)
-	} else {
-		fmt.Println(ok)
-	}
-	fmt.Println()
-	if ok, err := e.Enforce("user:ian", "dom:admin", "subscription:exhibition", "act:read"); err != nil {
-		fmt.Errorf("Enforce err: ", err)
-	} else {
-		fmt.Println(ok)
-	}
+	// fmt.Println()
+	// r1, _ := e.GetRolesForUser("user:ian", "dom:marketing")
+	// fmt.Println("GetRolesForUser: ", r1)
 
-	if _, err := e.AddNamedGroupingPolicy("g", []string{"user:sonnie", "role:organiser", "dom:guest"}); err != nil {
-		fmt.Errorf("e.AddNamedGroupingPolicy", err)
-	}
+	// fmt.Println()
+	// if ok, err := e.Enforce("user:ian", "dom:admin", "obj:account_admin", "act:read"); err != nil {
+	// 	fmt.Errorf("Enforce err: ", err)
+	// } else {
+	// 	fmt.Println(ok)
+	// }
+	// fmt.Println()
+	// if ok, err := e.Enforce("user:ian", "dom:admin", "subscription:exhibition", "act:read"); err != nil {
+	// 	fmt.Errorf("Enforce err: ", err)
+	// } else {
+	// 	fmt.Println(ok)
+	// }
+
+	// if _, err := e.AddNamedGroupingPolicy("g", []string{"user:sonnie", "role:organiser", "dom:guest"}); err != nil {
+	// 	fmt.Errorf("e.AddNamedGroupingPolicy", err)
+	// }
 	fmt.Println()
 	if ok, err := e.Enforce("user:sonnie", "dom:guest", "obj:news", "act:create_limited"); err != nil {
 		fmt.Println("Enforce err: ", err)
